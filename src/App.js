d2803efaba648c78c0b357172b542ed7e64a0f59
hybr1d <hybrid1006@gmail.com>
2024-10-11 00:32:05 +0900

    fetch movies

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import { useState, useEffect } from "react";
 
 function App() {
-    const [toDo, setToDo] = useState("");
-    const [toDos, setToDos] = useState([]);
-    const onChange = (event) => setToDo(event.target.value);
-    const onSubmit = (event) => {
-        event.preventDefault();
-        if (toDo === "") {
-            return;
-        }
-        setToDos((currentArray) => [toDo, ...currentArray]);
-        //setToDos((currentArray) => [...currentArray, toDo]); // 이렇게도 가능
-        setToDo("");
+    const [loading, setLoading] = useState(true);
+    const [movies, setMovies] = useState([]);
+    const getMoveis = async () => {
+        const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")).json();
+        setMovies(json.data.movies);
+        setLoading(false);
     };
+    useEffect(() => {
+        getMoveis();
+    }, []);
+
     return (
         <div>
-            <h1>My To Dos {toDos.length}</h1>
-            <form onSubmit={onSubmit}>
-                <input onChange={onChange} value={toDo} text="text" placeholder="Write your to do..."></input>
-                <button>Add To Do</button>
-            </form>
-            <hr />
-            {toDos.map((item, index) => (
-                <li key={index}>{item}</li>
-            ))}
+            {loading ? (
+                <h1>Loading...</h1>
+            ) : (
+                <div>
+                    {movies.map((movie) => (
+                        <div key={movie.id}>
+                            <img src={movie.medium_cover_image}></img>
+                            <h2>{movie.title}</h2>
+                            <p>{movie.summary}</p>
+                            <ul>
+                                {movie.genres.map((g) => (
+                                    <li key={g}>{g}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     );
 }
