14978add0481dcbf85b3fb4d3d64162b1365113e
hybird1006 <hybrid1006@gmail.com>
2024-10-10 21:44:41 +0900

    array status

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+    const [toDo, setToDo] = useState("");
+    const [toDos, setToDos] = useState([]);
+    const onChange = (event) => setToDo(event.target.value);
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if (toDo === "") {
+            return;
+        }
+        setToDos((currentArray) => [toDo, ...currentArray]);
+        //setToDos((currentArray) => [...currentArray, toDo]); // 이렇게도 가능
+        setToDo("");
+    };
+    console.log(toDos);
     return (
         <div>
-            <h1 className={styles.title}>Welcome back!!!</h1>
-            <Button text={"Continue"} />
+            <h1>My To Dos {toDos}</h1>
+            <form onSubmit={onSubmit}>
+                <input onChange={onChange} value={toDo} text="text" placeholder="Write your to do..."></input>
+                <button>Add To Do</button>
+            </form>
         </div>
     );
 }
