0afc5b15f1ef3a68f73e551075852013c49a7386
hybr1d <hybrid1006@gmail.com>
2024-10-13 15:12:34 +0900

    basename is repository name

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
-const router = createBrowserRouter([
+const router = createBrowserRouter(
     {
-        basename: `${process.env.PUBLIC_URL}`,
+        basename: "/react-for-beginners",
     },
-    {
-        path: "/",
-        element: <Home />,
-    },
-    {
-        path: "/movie/:id",
-        element: <Detail />,
-    },
-]);
+    [
+        {
+            path: "/",
+            element: <Home />,
+        },
+        {
+            path: "/movie/:id",
+            element: <Detail />,
+        },
+    ]
+);
 function App() {
     console.log(router);
     return <RouterProvider router={router} />;
