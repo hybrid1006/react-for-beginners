af62ccdedac5a1337d096d9d90f71e8e123e8e1c
hybr1d <hybrid1006@gmail.com>
2024-10-13 15:01:18 +0900

    public url

--- src/App.js
+++ src/App.js
@@ -4,15 +4,19 @@ import Detail from "./routes/Detail";
 
 const router = createBrowserRouter([
     {
+        basename: `${process.env.PUBLIC_URL}`,
+    },
+    {
         path: "/",
         element: <Home />,
     },
     {
-        path: "/movie",
+        path: "/movie/:id",
         element: <Detail />,
     },
 ]);
 function App() {
+    console.log(router);
     return <RouterProvider router={router} />;
 }
 
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -20,7 +20,14 @@ function Home() {
             ) : (
                 <div>
                     {movies.map((movie) => (
-                        <Movie key={movie.id} coverImg={movie.medium_cover_image} title={movie.title} summary={movie.summary} genres={movie.genres} />
+                        <Movie
+                            id={movie.id}
+                            key={movie.id}
+                            coverImg={movie.medium_cover_image}
+                            title={movie.title}
+                            summary={movie.summary}
+                            genres={movie.genres}
+                        />
                     ))}
                 </div>
             )}
