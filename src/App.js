2c03d63154c5efa633a56a0d7c352a459fcfff5d
hybr1d <hybrid1006@gmail.com>
2024-10-13 12:10:48 +0900

    router practice

--- src/App.js
+++ src/App.js
@@ -1,39 +1,19 @@
-import { useState, useEffect } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Home />,
+    },
+    {
+        path: "/movie",
+        element: <Detail />,
+    },
+]);
 function App() {
-    const [loading, setLoading] = useState(true);
-    const [movies, setMovies] = useState([]);
-    const getMoveis = async () => {
-        const json = await (await fetch("https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year")).json();
-        setMovies(json.data.movies);
-        setLoading(false);
-    };
-    useEffect(() => {
-        getMoveis();
-    }, []);
-
-    return (
-        <div>
-            {loading ? (
-                <h1>Loading...</h1>
-            ) : (
-                <div>
-                    {movies.map((movie) => (
-                        <div key={movie.id}>
-                            <img src={movie.medium_cover_image}></img>
-                            <h2>{movie.title}</h2>
-                            <p>{movie.summary}</p>
-                            <ul>
-                                {movie.genres.map((g) => (
-                                    <li key={g}>{g}</li>
-                                ))}
-                            </ul>
-                        </div>
-                    ))}
-                </div>
-            )}
-        </div>
-    );
+    return <RouterProvider router={router} />;
 }
 
 export default App;
